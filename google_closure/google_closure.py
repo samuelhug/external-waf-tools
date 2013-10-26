4c32fea2b3297a38d689519ddcc671e443bdf23e
Samuel Hug <samuel.b.hug@gmail.com>
2013-10-26 12:14:13 -0700

    Changed paths to absolute

--- google_closure/google_closure.py
+++ google_closure/google_closure.py
@@ -17,22 +17,22 @@ def configure(ctx):
 
     ctx.load_external_tool('utils', 'utils')
 
-    tools_node = ctx.path.find_dir(os.path.relpath(tooldir))
+    tools_node = ctx.root.find_dir(tooldir)
     if not tools_node:
         ctx.fatal('Unable to locate the google_closure tool directory.')
-    ctx.env.CLOSURE_TOOLS = tools_node.relpath()
+    ctx.env.CLOSURE_TOOLS = tools_node.abspath()
 
     closure_library_node = tools_node.find_dir('closure-library')
     if not closure_library_node:
         ctx.fatal('Unable to locate the closure library directory.')
-    ctx.env.CLOSURE_LIBRARY = closure_library_node.relpath() 
+    ctx.env.CLOSURE_LIBRARY = closure_library_node.abspath() 
 
     # Locate closure library scripts
     closure_scripts_node = closure_library_node.find_dir('closure/bin/build')
     if not closure_scripts_node:
         ctx.fatal('''Unable to locate the closure library scripts directory.
 Note: Did you run `git submodule init` and `git submodule update`''')
-    ctx.env.CLOSURE_SCRIPTS = closure_scripts_node.relpath()
+    ctx.env.CLOSURE_SCRIPTS = closure_scripts_node.abspath()
 
     try:
         ctx.find_python_program('closurebuilder', path_list=[ctx.env.CLOSURE_SCRIPTS], var='CLOSURE_BUILDER')
